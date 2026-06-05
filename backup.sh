7eb017b1168b862a9b0d43ea939125ae88d6bdbc
realtimestert <istewpotter1@gmail.com>
2026-06-05 15:43:27 -0400

    fixed backup script

--- backup.sh
+++ backup.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-# Date stamp
 DATE=$(date +%Y%m%d-%H%M%S)
 
-# Backup directory
 BACKUP_DIR="$HOME/sanusbio-backups/$DATE"
-MYSQL_PWD=$(cat ~/.sanusbio-db-pass)
-
-# Create directory if it doesn't exist
 mkdir -p "$BACKUP_DIR"
+
+MYSQL_PWD=$(cat ~/.sanusbio-db-pass)
 
 echo "Creating database backup..."
 
-podman exec sanusbio-db mysqldump \
+podman exec -e MYSQL_PWD="$MYSQL_PWD" sanusbio-db mysqldump \
     --no-tablespaces \
     -u sanusbio \
-    -p"$MYSQL_PWD" \
     sanusbio \
     > "$BACKUP_DIR/sanusbio-database-$DATE.sql"
 
@@ -23,8 +19,6 @@ echo "Creating uploads backup..."
 
 tar -czf "$BACKUP_DIR/sanusbio-uploads-$DATE.tar.gz" \
     -C "$HOME/.local/share/containers/storage/volumes/sanusbio-app_uploads_data/_data" .
-
-clear
 
 echo "Backup complete."
 echo "Files saved to:"
