b29de847418f36075023f36ab8bb8a5a5ce6754e
realtimestert <istewpotter1@gmail.com>
2026-08-01 08:32:20 -0400

    Minor changes

--- podrestore.sh
+++ podrestore.sh
@@ -8,6 +8,3 @@ podman-compose down && podman-compose up -d --build --force-recreate
 # Executing the following will load new sql data that was written
 # This can be modified as needed for new sql files that are added to the init directory
 podman exec -e MYSQL_PWD="$(cat ~/.sanusbio-db-pass)" -i sanusbio-db mysql -u sanusbio sanusbio < init/
-
-# For Google Drive backup via rclone
-rclone copy ~/sanusbio-backups gdrive:"Server Backups"
